c2d4243e27b159eeaee5f61cc2a3b78c96dd29e9
dabi <dabich626626@gmail.com>
2023-10-09 15:12:58 +0200

    ver5

--- src/main/java/com/example/demo/ExamService.java
+++ src/main/java/com/example/demo/ExamService.java
@@ -1,4 +1,32 @@
 package com.example.demo;
 
-public class ExamService  {
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+public class ExamService implements ExaminerService {
+
+private final QuestionService service;
+
+    public ExamService(QuestionService service) {
+        this.service = service;
+    }
+
+    public Collection<Question> getQuestions(int amount){
+
+        var allQuestions = service.getAll();
+        if(amount > allQuestions.size()){
+
+            throw new NotTheRightAmountException();
+        }
+
+        Set<Question> result = new HashSet<>();
+        while (result.size() != amount){
+
+            result.add(service.getRandomQuestion());
+
+        }
+
+        return result;
+    }
 }
--- src/main/java/com/example/demo/ExaminerService.java
+++ src/main/java/com/example/demo/ExaminerService.java
@@ -6,27 +6,5 @@ import java.util.HashSet;
 public interface ExaminerService {
 
 
-    public void  getAll(int amount){
-
-        for (int i = 0; i <= question.length; i++) {
-
-            return question[i];
-        }
-
-
-    }
-
-
-public static void getallQuestions(int amount){
-
-    Question question = new Question();
-
-
-    if (amount >  getQuestions.size()){
-
-        throw new NotTheRightAmountException();
-    }
-}
-
 
 }
